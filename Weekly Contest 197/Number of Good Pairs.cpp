1398108679f56ae9b1d166d2c7167f10d7210890
Shambhavi Sahay <41591299+iamshambhavi@users.noreply.github.com>
2020-07-13 21:29:11 +0530

    Create Number of Good Pairs.cpp

---
+++ Weekly Contest 197/Number of Good Pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int>m;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            count+=m[nums[i]]++;
+        }
+        return count;
+    }
+};
